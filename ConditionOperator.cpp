c26886d6b67712e799790c0f5a5d169d91854549
Masud Rana <masud.mr5622@gmail.com>
2025-03-03 12:05:34 +0800

    Add New Number.

--- ConditionOperator.cpp
+++ ConditionOperator.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 int main()
 {
-    int a, b, max;
+    int a, b, max, Maxnumber;
     cin >> a >> b;
+    // General if elas Condition
     if (a > b)
     {
         max = a;
@@ -13,6 +14,13 @@ int main()
     {
         max = b;
     }
-    cout << "Max Number Is: " << max;
+    cout << "Max Number Is: " << max << endl;
+
+    // ternary operator
+
+    Maxnumber = a > b ? a : b;
+
+    cout << "Max Number Is: " << Maxnumber << endl;
+
     return 0;
 }
