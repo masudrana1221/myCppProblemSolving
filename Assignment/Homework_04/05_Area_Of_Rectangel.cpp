53ac71abc5866d44947b89e9aea8d5bfc9eba59a
Masud Rana <masud.mr5622@gmail.com>
2025-05-16 01:21:09 +0800

    add my homework of cpp programming

---
+++ Assignment/Homework_04/05_Area_Of_Rectangel.cpp
@@ -0,0 +1,18 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Deffain a User Function
+double Area(float a, float b)
+{
+    double areaOf = a * b;
+    return areaOf;
+}
+int main()
+{
+    float Height, Width;
+    Height = 4, Width = 3;
+    // Call the Function AnD Get output
+    cout << "The Area OF Rectangle is: " << fixed << setprecision(2) << Area(Height, Width);
+    return 0;
+}
